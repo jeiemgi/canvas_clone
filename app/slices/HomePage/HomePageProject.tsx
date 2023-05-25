e0eddcb16c01dbfc99ab443ab2c21637c4c39cce
José Gutiérrez <hello@josegtz.com>
2023-05-25 08:59:02 -0600

    feat: add case studies

--- app/slices/HomePage/HomePageProject.tsx
+++ app/slices/HomePage/HomePageProject.tsx
@@ -1,37 +1,92 @@
-import React from 'react';
-import aliveSample from "public/img/sample/alive-bg.png"
+import React from "react";
+import aliveSample from "public/img/sample/alive-bg.png";
+import ernestBaker from "public/img/sample/ernest-baker-bg.png";
 
-function HomePageProject() {
-    return (
-        <div
-            className={'w-full h-screen bg-cover flex flex-col justify-between relative'}
-            style={{backgroundImage: `url('${aliveSample}')`}}
-        >
-            <div className="grid-container">
-                <div className={'col-span-3 flex pt-8 justify-between items-end'}>
-                    <h3 className={'text-white heading--3'}> ALIVE <br/> CASE STUDY</h3>
-                    <h3 className={'text-white heading--3'}> 1 / 5</h3>
-                </div>
-            </div>
-
-            <div className="grid-container">
-                <div className="col-span-5 self-end py-8">
-                    <h3 className={'text-white heading--3'}> branding, app, Website</h3>
-                </div>
-            </div>
-            <div className="grid-container absolute h-[500px] bottom-0 overflow-hidden">
-                <div className={'col-span-4 col-start-9'}>
-                    <p className={'text-white body--2 mb-5'}>
-                        A description of the project. Maybe one to two sentences. More
-                        content goes here. Think it will be two to three lines.
-                    </p>
-                    <div className={'w-full h-[300px] bg-grey mb-5'}></div>
-                    <div className={'w-full h-[300px] bg-grey mb-5'}></div>
-                    <div className={'w-full h-[300px] bg-grey mb-5'}></div>
-                </div>
-            </div>
-        </div>
-    );
+interface HomepageProjectProps {
+  title: string;
+  capabilities: string;
+  description: string;
+  cta: string;
+  image: string;
 }
 
-export default HomePageProject;
+function HomePageProject({
+  title,
+  capabilities,
+  description,
+  cta,
+  image,
+}: HomepageProjectProps) {
+  return (
+    <div
+      className={
+        "relative flex h-[90vh] w-full flex-col justify-between bg-cover bg-center md:h-screen"
+      }
+      style={{ backgroundImage: `url('${image}')` }}
+    >
+      <div className="grid-container">
+        <div
+          className={
+            "col-span-4 flex items-end justify-between pt-5 md:col-span-3 md:pt-8"
+          }
+        >
+          <h3 className={"heading--3 whitespace-pre-line text-white"}>
+            {title}
+          </h3>
+          <h3 className={"desktop-only heading--3 text-white"}> 1 / 5</h3>
+        </div>
+      </div>
+
+      <div className={"mobile-only col-span-4"}>
+        <h3 className={"heading--3 text-center text-white"}>( {cta} )</h3>
+      </div>
+
+      <div className="grid-container bottom-0 overflow-hidden pb-28 md:absolute md:h-[500px] md:pb-0">
+        <div className="col-span-5 mb-11 self-end md:mb-0 md:py-8">
+          <h3 className={"heading--3 text-white"}>{capabilities}</h3>
+        </div>
+
+        <div className={"col-span-4 md:col-start-9"}>
+          <p className={"body--2 mb-5 text-white"}>{description}</p>
+          <div className="desktop-only">
+            <div className={"mb-5 h-[300px] w-full bg-grey"}></div>
+            <div className={"mb-5 h-[300px] w-full bg-grey"}></div>
+            <div className={"mb-5 h-[300px] w-full bg-grey"}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function HomePageProjects() {
+  return (
+    <section className={"relative"}>
+      <HomePageProject
+        cta={"VIEW PROJECT"}
+        title={"ALIVE \n CASE STUDY"}
+        capabilities={"Branding, App, Website"}
+        image={aliveSample}
+        description={
+          "A description of the project. Maybe one to two sentences. More goes here. Think it will be two to three lines."
+        }
+      />
+
+      <HomePageProject
+        cta={"VIEW PROJECT"}
+        title={"ERNEST W. BAKER \n CASE STUDY"}
+        capabilities={"Ecommerce Website"}
+        image={ernestBaker}
+        description={
+          "A description of the project. Maybe one to two sentences. More goes here. Think it will be two to three lines."
+        }
+      />
+
+      <div className={"mobile-only absolute right-4 top-5"}>
+        <h3 className={"heading--3 text-white"}> 1 / 5</h3>
+      </div>
+    </section>
+  );
+}
+
+export default HomePageProjects;
