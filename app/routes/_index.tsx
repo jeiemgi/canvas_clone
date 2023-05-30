c7011c2682ffbeb2eb3e5cfcaa826877056995cf
José Gutiérrez <hello@josegtz.com>
2023-05-30 11:04:03 -0600

    feat: Add Prismic Client

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,13 +6,25 @@ import HomePageProjects from "~/slices/HomePage/HomePageProject";
 import HomePageQuote from "~/slices/HomePage/HomePageQuote";
 import HomePageReviews from "~/slices/HomePage/HomePageReviews";
 import background from "public/img/home-background.png";
+import { client } from "~/lib/prismicClient";
 import type { V2_MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Canvas Studio Website V4" }];
 };
 
-export default function _index() {
+export const loader = async () => {
+  const data = await client.get("homepage");
+  return {
+    data,
+  };
+};
+
+export default function HomePage() {
+  const data = useLoaderData();
+  console.log(data);
+
   return (
     <Layout>
       <main>
