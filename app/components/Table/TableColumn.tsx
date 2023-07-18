c896e7a9230f631c31fed1202d5dc1d21d0b5759
José Gutiérrez <hello@josegtz.com>
2023-07-18 16:22:43 -0600

    feat: table column

--- app/components/Table/TableColumn.tsx
+++ app/components/Table/TableColumn.tsx
@@ -27,7 +27,7 @@ export function TableColumn({
       <h3 className={"heading--2 mb-5"}>{title}</h3>
       <p
         className={
-          "body--3 mb-6 h-[200px] whitespace-pre-line md:max-w-[22rem]"
+          "body--3 mb-6 whitespace-pre-line md:h-[200px] md:max-w-[22rem]"
         }
       >
         {description}
@@ -43,7 +43,7 @@ export function TableColumn({
             key={`${row}-${index}`}
             className={clsx(
               "body--3 relative w-full border-b border-black/30 py-2.5 md:border-0",
-              "group-first:after:absolute group-first:after:bottom-0 group-first:after:block group-first:after:h-[1px] group-first:after:w-[500%] group-first:after:bg-black/30"
+              "md:group-first:after:absolute md:group-first:after:bottom-0 md:group-first:after:block md:group-first:after:h-[1px] md:group-first:after:w-[500%] md:group-first:after:bg-black/30"
             )}
           >
             {row}
