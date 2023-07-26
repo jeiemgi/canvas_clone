209d5d862155e057728bf8195596bf73096f9c4a
José Gutiérrez <hello@josegtz.com>
2023-07-26 13:48:22 -0600

    feat: Add disabled state

--- app/slices/Contact/ContactForm.tsx
+++ app/slices/Contact/ContactForm.tsx
@@ -1,25 +1,41 @@
-import React from "react";
+import { useState } from "react";
 import { Input, TextArea } from "~/components/Input";
-import { PrimaryCTALink } from "~/components/CTA";
+import { PrimaryCTAButton } from "~/components/CTA";
 
 interface Props {}
 
 function ContactForm(props: Props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
+
+  const isDisabled =
+    !name || name !== "" || !email || email !== "" || !text || text !== "";
+
   return (
     <form>
       <Input
+        value={name}
+        // @ts-ignore
+        onChange={(e) => setName(e.target.value)}
         label={"Full Name*"}
         type="text"
         id={"full-name"}
         containerClassName={"mb-5"}
       />
       <Input
+        value={email}
+        // @ts-ignore
+        onChange={(e) => setEmail(e.target.value)}
         label={"Email*"}
         type="text"
         id={"email"}
         containerClassName={"mb-5"}
       />
       <TextArea
+        value={text}
+        // @ts-ignore
+        onChange={(e) => setText(e.target.value)}
         label={"Project Detail*"}
         type="textarea"
         id={"project-detail"}
@@ -29,9 +45,9 @@ function ContactForm(props: Props) {
         containerClassName={"mb-10"}
       />
       <div className={"flex justify-end"}>
-        <PrimaryCTALink dark to={"#"} size={"lg"}>
+        <PrimaryCTAButton disabled={isDisabled} dark size={"lg"}>
           Send
-        </PrimaryCTALink>
+        </PrimaryCTAButton>
       </div>
     </form>
   );
